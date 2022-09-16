50a0ea9584d8577c76eebf112828e08fc6faf297
Everaldo <torresjr1984@hotmail.com>
2022-09-16 12:58:05 -0300

    implementada toda a parte de front-end

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[EventController::class, 'index']);
-Route::get('/events/create',[EventController::class, 'create']);
-Route::get('/events/{id}',[EventController::class, 'show']);
-Route::post('/events',[EventController::class, 'store']);
-
-Route::get('/login', [AuthController::class, 'index'])->name('login');
-Route::post('/login', [AuthController::class, 'login_action'])->name('user.login_action');
-
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'register_action'])->name('user.register_action');
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 
 Route::get('/contato', function () {
     return view('contact');
 });
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}',[EventController::class, 'joinEvent'])->middleware('auth'); 
